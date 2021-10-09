0a315ac07bd8cbb501f53c04cdb4321e302cd07d
Ilesanmi Omoniyi O <31474007+omoniyi24@users.noreply.github.com>
2021-10-09 11:42:51 +0100

    Add files via upload

---
+++ src/main/java/com/omoniyi/codetest/interview/config/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.ominiyi.techquest.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
+	    registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+	    registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+	    registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+	    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.omoniyi.codetest.interview.web.rest"))
+                .paths(PathSelectors.regex("/*.*"))
+                .build()
+                .apiInfo(metaData());
+    }
+	
+	private ApiInfo metaData() {
+        return new ApiInfo(
+                "Interview Test",
+                "Spring Boot REST API",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Omoniyi Olasunkanmi", "", "omoniyi24@gmail.com"),
+               "API License Version 1.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+        		Collections.emptyList());
+        
+    }
+}
